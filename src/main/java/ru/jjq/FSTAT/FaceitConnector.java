29656170f95869e4d99a349ae69a0843dfcf6de0
Jok1r <jok1r30@yandex.ru>
2024-11-14 00:31:01 +0300

    More data abouts player, stat

--- src/main/java/ru/jjq/FSTAT/FaceitConnector.java
+++ src/main/java/ru/jjq/FSTAT/FaceitConnector.java
@@ -14,6 +14,7 @@ public class FaceitConnector {
 
     public String faceit_api = "https://open.faceit.com/data/v4";
     public String players = "/players";
+    public String stats_of_cs2 = "/games/cs2/stats";
 
     public Player getPlayer(String name) {
         while (true) {
@@ -26,8 +27,30 @@ public class FaceitConnector {
 
                 String body = IOUtils.readInputStream(urlConnection.getInputStream());
                 JsonObject asJsonObject = JsonParser.parseString(body).getAsJsonObject();
-                System.out.println(asJsonObject.toString());
+                System.out.println("Player: " + asJsonObject.toString());
                 return new Player(asJsonObject);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new Player();
+    }
+
+
+    public Player getPlayerStats(Player player) {
+        while (true) {
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) IOUtils.newConnection(faceit_api + players + "/" + player.player_id + stats_of_cs2);
+                int response_code = urlConnection.getResponseCode();
+                if(response_code / 100 != 2) {
+                    break;
+                }
+
+                String body = IOUtils.readInputStream(urlConnection.getInputStream());
+                JsonObject asJsonObject = JsonParser.parseString(body).getAsJsonObject();
+                System.out.println("Stats CS2: " + asJsonObject);
+                return player;
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/main/java/ru/jjq/FSTAT/controller/PlayerController.java
+++ src/main/java/ru/jjq/FSTAT/controller/PlayerController.java
@@ -13,6 +13,7 @@ public class PlayerController {
     @GetMapping(value = "/{name}")
     public String player(@PathVariable(name = "name") String name, Model model) {
         Player player = FaceitConnector.get().getPlayer(name);
+        player = FaceitConnector.get().getPlayerStats(player);
         model.addAttribute("nickname", player.nickname);
         model.addAttribute("avatar", player.avatar);
         model.addAttribute("cover_image", player.cover_image);
